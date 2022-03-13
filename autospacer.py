61068e91a9e8227ab7a67734c6cf4cfbabe0dfff
Sammi De Guzman <sammi@sammdot.ca>
2022-03-13 13:46:58 -0700

    Initial commit

---
+++ autospacer.py
@@ -0,0 +1,29 @@
+from threading import Thread
+
+from plover.steno import Stroke
+from plover.translation import Translation
+
+class AutoSpacer(Thread):
+
+  def __init__(self, engine):
+    super().__init__()
+    self.engine = engine
+    self.engine.hook_connect("config_changed", self._on_config_changed)
+
+  def start(self):
+    self._on_config_changed(None)
+    super().start()
+
+  def stop(self):
+    pass
+
+  @staticmethod
+  def needs_zero_space(system):
+    return "Japanese" in system or "Chinese" in system
+
+  def _on_config_changed(self, _):
+    tl = Translation([Stroke([])],
+      "{mode:set_space:}"
+      if AutoSpacer.needs_zero_space(self.engine.config["system_name"]) else
+      "{mode:reset_space}")
+    self.engine._translator.translate_translation(tl)
